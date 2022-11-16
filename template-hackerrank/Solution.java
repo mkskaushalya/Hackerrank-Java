92070c4dfccb2c5c3118999a03700c922d2080f4
Sahan Kaushalya <mkskaushalya@gmail.com>
2022-11-16 23:27:50 +0530

    Update Solution.java

--- template-hackerrank/Solution.java
+++ template-hackerrank/Solution.java
@@ -1,44 +1,5 @@
-/*In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes. An if-else statement has the following logical flow:
-Task
-Given an integer,n , perform the following conditional actions:
-
-If n is odd, print Weird
-If n is even and in the inclusive range of 2 to 5, print Not Weird
-If n is even and in the inclusive range of 6 to 20, print Weird
-If n is even and greater than 20, print Not Weird
-Complete the stub code provided in your editor to print whether or not n is weird.
-
-Input Format
-
-A single line containing a positive integer, n.
-
-Constraints
-1 <= n <= 10
-Output Format
-
-Print Weird if the number is weird; otherwise, print Not Weird.
-
-Sample Input 0
-
-3
-Sample Output 0
-
-Weird
-
-Sample Input 1
-
-24
-Sample Output 1
-
-Not Weird
-
-Explanation
-
-Sample Case 0: n = 3 
-n is odd and odd numbers are weird, so we print Weird.
-
-Sample Case 1: n = 24
-n > 20 and n is even, so it isn't weird. Thus, we print Not Weird.
+/*
+Add hackerrank questin here
  */
 
 
@@ -46,10 +7,13 @@ n > 20 and n is even, so it isn't weird. Thus, we print Not Weird.
 
 
 /*
- * Owner in this file
- * Sahan Kaushalya
- * follow my github and social
- * https://github.com/mkskaushalya
- * https://github.com/mkskaushalya/Hackerrank-Java
- * https://www.linkedin.com/in/sahankaushalya/
+ * ========================================================================================
+ * =======                           Owner in this file                             =======
+ * =======                            Sahan Kaushalya                               =======
+ * =======                      follow my github and social                         =======
+ * =======--------------------------------------------------------------------------=======
+ * ======= Github           :   https://github.com/mkskaushalya                     =======
+ * ======= Github Repostory :   https://github.com/mkskaushalya/Hackerrank-Java     =======
+ * ======= LinkedIn         :   https://www.linkedin.com/in/sahankaushalya/         =======
+ * ========================================================================================
  */
